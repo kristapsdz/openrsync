23c6ee13c9767b7031a64f9bd48a423bdecdf8ca
kristapsdz <kristaps@bsd.lv>
2019-01-18 21:02:44 +0000

    Initial functionality for recursive file creation.  This is *not* safe or secure, and does not handle most regular cases.  It simply paves an initial framework.

--- receiver.c
+++ receiver.c
@@ -74,6 +74,28 @@ init_blk(struct blk *p, const struct blkset *set, off_t offs,
 	hash_slow(map + offs, p->len, p->chksum_long, sess);
 }
 
+static int
+process_dir(const struct opts *opts, int fdin, int fdout, int root, 
+	const struct flist *f, size_t idx, const struct sess *sess)
+{
+	const char	*path;
+
+	path = f->path; /* TODO */
+	assert('\0' != path[0]);
+
+	if (opts->dry_run)
+		return 1;
+
+	if (-1 == mkdirat(root, path, 0755))
+		if (EEXIST != errno) {
+			WARN1(opts, "openat: %s", path);
+			return 0;
+		}
+
+	LOG3(opts, "created: %s", path);
+	return 1;
+}
+
 /*
  * Open the existing file (if found), the temporary file, read new data
  * (and good blocks) from the sender, reconstruct the file, and rename
@@ -83,7 +105,7 @@ init_blk(struct blk *p, const struct blkset *set, off_t offs,
  * Return zero on failure, non-zero on success.
  */
 static int
-process(const struct opts *opts, int fdin, int fdout, int root, 
+process_file(const struct opts *opts, int fdin, int fdout, int root, 
 	const struct flist *f, size_t idx, const struct sess *sess,
 	size_t csumlen)
 {
@@ -97,18 +119,27 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 	uint32_t	 hash;
 	struct timeval	 tv[2];
 	mode_t		 perm;
+	const char	*path;
 
 	/* 
 	 * Not having a file is fine: it just means that we'll need to
 	 * download the full file (i.e., have zero blocks).
 	 * If this is the case, map will stay at MAP_FAILED.
+	 * If we're recursive, then ignore the absolute indicator.
 	 */
 
-	if (-1 == (ffd = openat(root, f->path, O_RDONLY, 0))) {
+	if (opts->recursive)
+		path = f->path; /* TODO */
+	else
+		path = f->filename;
+
+	assert('\0' != path[0]);
+
+	if (-1 == (ffd = openat(root, path, O_RDONLY, 0))) {
 		if (ENOENT == errno)
-			WARN2(opts, "openat: %s", f->path);
+			WARN2(opts, "openat: %s", path);
 		else
-			WARN1(opts, "openat: %s", f->path);
+			WARN1(opts, "openat: %s", path);
 	}
 
 	/*
@@ -120,10 +151,10 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 
 	if (-1 != ffd) {
 		if (-1 == fstat(ffd, &st)) {
-			WARN(opts, "fstat: %s", f->path);
+			WARN(opts, "fstat: %s", path);
 			goto out;
 		} else if ( ! S_ISREG(st.st_mode)) {
-			WARNX(opts, "not a regular file: %s", f->path);
+			WARNX(opts, "not a regular file: %s", path);
 			goto out;
 		} 
 
@@ -131,7 +162,7 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 
 		if (st.st_size == f->st.size &&
 		    st.st_mtime == f->st.mtime) {
-			LOG3(opts, "%s: skipping file", f->path);
+			LOG3(opts, "%s: skipping file", path);
 			return 1;
 		} 
 	}
@@ -172,7 +203,7 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 		map = mmap(NULL, mapsz, PROT_READ, MAP_SHARED, ffd, 0);
 
 		if (MAP_FAILED == map) {
-			WARN(opts, "mmap: %s", f->path);
+			WARN(opts, "mmap: %s", path);
 			goto out;
 		}
 
@@ -189,10 +220,10 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 			init_blk(&p->blks[i], p, offs, i, map, sess);
 
 		LOG3(opts, "%s: mapped %llu B with %zu "
-			"blocks", f->path, p->size, p->blksz);
+			"blocks", path, p->size, p->blksz);
 	} else {
 		p->len = MAX_CHUNK;
-		LOG3(opts, "%s: not mapped", f->path);
+		LOG3(opts, "%s: not mapped", path);
 	}
 
 	/* 
@@ -203,12 +234,22 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 	 */
 
 	hash = arc4random();
-	if (asprintf(&tmpfile, ".%s.%" PRIu32, f->path, hash) < 0) {
-		ERR(opts, "asprintf");
-		tmpfile = NULL;
-		goto out;
-	} 
 
+	if (opts->recursive) {
+		if (asprintf(&tmpfile, "%.*s/.%s.%" PRIu32, 
+		    (int)f->dirlen, path, 
+		    path + f->dirlen + 1, hash) < 0) {
+			ERR(opts, "asprintf");
+			tmpfile = NULL;
+			goto out;
+		} 
+	} else {
+		if (asprintf(&tmpfile, ".%s.%" PRIu32, path, hash) < 0) {
+			ERR(opts, "asprintf");
+			tmpfile = NULL;
+			goto out;
+		} 
+	}
 	/* 
 	 * If we have -p, then copy over the file's mode only if we're
 	 * updating an existing file, not making one anew.
@@ -227,11 +268,11 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 		goto out;
 	}
 
-	LOG3(opts, "%s: temporary: %s", f->path, tmpfile);
+	LOG3(opts, "%s: temporary: %s", path, tmpfile);
 
 	/* Now transmit the metadata for set and blocks. */
 
-	if ( ! blk_send(opts, fdout, csumlen, p, f->path)) {
+	if ( ! blk_send(opts, fdout, csumlen, p, path)) {
 		ERRX1(opts, "blk_send");
 		goto out;
 	} else if ( ! blk_send_ack(opts, fdin, p, idx)) {
@@ -245,7 +286,7 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 	 * rename as the original file.
 	 */
 
-	if ( ! blk_merge(opts, fdin, ffd, p, tfd, f->path, map, mapsz)) {
+	if ( ! blk_merge(opts, fdin, ffd, p, tfd, path, map, mapsz)) {
 		ERRX1(opts, "blk_merge");
 		goto out;
 	}
@@ -258,16 +299,16 @@ process(const struct opts *opts, int fdin, int fdout, int root,
 		tv[1].tv_sec = f->st.mtime;
 		tv[1].tv_usec = 0;
 		if (-1 == futimes(tfd, tv)) {
-			ERR(opts, "futimes: %s", f->path);
+			ERR(opts, "futimes: %s", path);
 			goto out;
 		}
-		LOG3(opts, "matching futimes: %s", f->path);
+		LOG3(opts, "matching futimes: %s", path);
 	}
 
 	/* Finally, rename the temporary to the real file. */
 
-	if (-1 == renameat(root, tmpfile, root, f->path)) {
-		ERR(opts, "renameat: %s, %s", tmpfile, f->path);
+	if (-1 == renameat(root, tmpfile, root, path)) {
+		ERR(opts, "renameat: %s, %s", tmpfile, path);
 		goto out;
 	}
 
@@ -342,7 +383,7 @@ rsync_receiver(const struct opts *opts, const struct sess *sess,
 	struct flist	*fl = NULL;
 	size_t		 i, flsz = 0, csum_length = CSUM_LENGTH_PHASE1;
 	char		*tofree;
-	int		 rc = 0, dfd = -1, phase = 0;
+	int		 rc = 0, dfd = -1, phase = 0, c;
 	int32_t	 	 ioerror;
 
 	if (-1 == pledge("unveil rpath cpath wpath stdio fattr", NULL)) {
@@ -428,12 +469,15 @@ rsync_receiver(const struct opts *opts, const struct sess *sess,
 	LOG2(opts, "receiver ready for %zu-checksum "
 		"data: %s", csum_length, root);
 
-	for (i = 0; i < flsz; i++)
-		if ( ! process(opts, fdin, fdout, 
-		    dfd, &fl[i], i, sess, csum_length)) {
-			ERRX1(opts, "process");
+	for (i = 0; i < flsz; i++) {
+		c = S_ISDIR(fl[i].st.mode) ?
+			process_dir(opts, fdin, fdout, 
+				dfd, &fl[i], i, sess) :
+			process_file(opts, fdin, fdout, 
+				dfd, &fl[i], i, sess, csum_length);
+		if ( ! c) 
 			goto out;
-		}
+	}
 
 	/* Properly close us out by progressing through the phases. */
 
