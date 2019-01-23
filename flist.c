a71189dc29d262d807c19b19fb592d73848ed900
kristaps <>
2019-01-23 10:33:06 +0000

    Print time_t as intmax_t.

--- flist.c
+++ flist.c
@@ -152,9 +152,9 @@ flist_send(struct sess *sess, int fd,
 		flag = FLIST_NAME_LONG;
 
 		LOG3(sess, "sending file metadata: %s "
-			"(size %jd, mtime %lld, mode %o)",
+			"(size %jd, mtime %jd, mode %o)",
 			fn, (intmax_t)f->st.size, 
-			f->st.mtime, f->st.mode);
+			(intmax_t)f->st.mtime, f->st.mode);
 
 		/* Now write to the wire. */
 
@@ -409,9 +409,9 @@ flist_recv(struct sess *sess, int fd, size_t *sz)
 		} 
 
 		LOG3(sess, "received file metadata: %s "
-			"(size %jd, mtime %lld, mode %o)",
+			"(size %jd, mtime %jd, mode %o)",
 			ff->path, (intmax_t)ff->st.size, 
-			ff->st.mtime, ff->st.mode);
+			(intmax_t)ff->st.mtime, ff->st.mode);
 	}
 
 	if (0 == flsz) {
