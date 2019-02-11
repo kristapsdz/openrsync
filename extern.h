d3e3b554c0f0a88cb3af54be6dabe1c8e5e02021
kristapsdz <kristaps@bsd.lv>
2019-02-11 20:45:22 +0000

    Enable groupid in the protocol.  This is not linked to the system itself yet.

--- extern.h
+++ extern.h
@@ -104,6 +104,7 @@ struct	opts {
 	int		 preserve_times; /* -t */
 	int		 preserve_perms; /* -p */
 	int		 preserve_links; /* -l */
+	int		 preserve_gids; /* -g */
 	int		 del; /* --delete */
 	const char	*rsync_path; /* --rsync-path */
 };
