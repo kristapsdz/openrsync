7364a24877b2eab26d176931f22282a195eecb5a
kristapsdz <kristaps@bsd.lv>
2019-02-19 08:04:11 +0000

    As found by benno@, if --numeric-ids is passed in, rsync does not send or receive the uid/gid lists at all.  This also means that we need not process the lists, as we're simply going to copy around the same value.

--- ids.c
+++ ids.c
@@ -52,6 +52,8 @@ idents_assign_gid(struct sess *sess, struct flist *fl, size_t flsz,
 {
 	size_t	 i, j;
 
+	assert(!sess->opts->numeric_ids);
+
 	for (i = 0; i < flsz; i++) {
 		if (fl[i].st.gid == 0)
 			continue;
@@ -71,6 +73,8 @@ idents_assign_uid(struct sess *sess, struct flist *fl, size_t flsz,
 	const struct ident *ids, size_t idsz)
 {
 	size_t	 i, j;
+
+	assert(!sess->opts->numeric_ids);
 
 	for (i = 0; i < flsz; i++) {
 		if (fl[i].st.uid == 0)
@@ -99,14 +103,7 @@ idents_remap(struct sess *sess, int isgid, struct ident *ids, size_t idsz)
 	struct passwd	*usr;
 	int32_t		 id;
 
-	if (sess->opts->numeric_ids) {
-		for (i = 0; i < idsz; i++) {
-			assert(ids[i].id != 0);
-			ids[i].mapped = ids[i].id;
-		}
-		LOG4(sess, "did not remap identifiers");
-		return;
-	}
+	assert(!sess->opts->numeric_ids);
 
 	for (i = 0; i < idsz; i++) {
 		assert(ids[i].id != 0);
