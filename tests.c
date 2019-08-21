e9ec084ca52d9edcbfbf1fb480e0727721d564f9
kristapsdz <kristaps@bsd.lv>
2019-08-21 11:19:36 +0000

    Use newest oconfigure.

--- tests.c
+++ tests.c
@@ -377,8 +377,18 @@ int
 main(void)
 {
 	struct fooq foo_q;
+	struct foo *p, *tmp;
+	int i = 0;
 
 	TAILQ_INIT(&foo_q);
+
+	/*
+	 * Use TAILQ_FOREACH_SAFE because some systems (e.g., Linux)
+	 * have TAILQ_FOREACH but not the safe variant.
+	 */
+
+	TAILQ_FOREACH_SAFE(p, &foo_q, entries, tmp)
+		p->bar = i++;
 	return 0;
 }
 #endif /* TEST_SYS_QUEUE */
