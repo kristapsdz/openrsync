e82f7827180bac3759efea748a093907eda6afdc
kristapsdz <kristaps@bsd.lv>
2020-03-10 21:26:09 +0000

    Fix compatibility functions.

--- compats.c
+++ compats.c
@@ -879,13 +879,14 @@ memrchr(const void *s, int c, size_t n)
 #if !HAVE_MKFIFOAT
 #include <sys/stat.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 int
 mkfifoat(int fd, const char *path, mode_t mode)
 {
-	int	curfd, newfd;
+	int	er, curfd = -1, newfd = -1;
 
 	/* Get our current directory then switch to the given one. */
 
@@ -893,30 +894,45 @@ mkfifoat(int fd, const char *path, mode_t mode)
 		if ((curfd = open(".", O_RDONLY | O_DIRECTORY, 0)) == -1)
 			return -1;
 		if (fchdir(fd) == -1)
-			return -1;
+			goto out;
 	}
 
 	if ((newfd = mkfifo(path, mode)) == -1)
-		return -1;
+		goto out;
 
-	/* This leaves the file if it fails. */
+	/* This leaves the fifo if it fails. */
 
-	if (fd != AT_FDCWD && fchdir(curfd) == -1)
-		return -1;
+	if (curfd != -1 && fchdir(curfd) == -1)
+		goto out;
+	if (curfd != -1)
+		close(curfd);
 
 	return newfd;
+out:
+	/* Ignore errors in close(2). */
+
+	er = errno;
+	if (curfd != -1)
+		fchdir(curfd);
+	if (curfd != -1)
+		close(curfd);
+	if (newfd != -1)
+		close(newfd);
+	errno = er;
+	return -1;
 }
 #endif /* !HAVE_MKFIFOAT */
 #if !HAVE_MKNODAT
 #include <sys/stat.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 int
 mknodat(int fd, const char *path, mode_t mode, dev_t dev)
 {
-	int	curfd, newfd;
+	int	er, curfd = -1, newfd = -1;
 
 	/* Get our current directory then switch to the given one. */
 
@@ -924,18 +940,33 @@ mknodat(int fd, const char *path, mode_t mode, dev_t dev)
 		if ((curfd = open(".", O_RDONLY | O_DIRECTORY, 0)) == -1)
 			return -1;
 		if (fchdir(fd) == -1)
-			return -1;
+			goto out;
 	}
 
 	if ((newfd = mknod(path, mode, dev)) == -1)
-		return -1;
+		goto out;
 
 	/* This leaves the node if it fails. */
 
-	if (fd != AT_FDCWD && fchdir(curfd) == -1)
-		return -1;
+	if (curfd != -1 && fchdir(curfd) == -1)
+		goto out;
+	if (curfd != -1)
+		close(curfd);
 
 	return newfd;
+out:
+	
+	/* Ignore errors in close(2). */
+
+	er = errno;
+	if (curfd != -1)
+		fchdir(curfd);
+	if (curfd != -1)
+		close(curfd);
+	if (newfd != -1)
+		close(newfd);
+	errno = er;
+	return -1;
 }
 #endif /* !HAVE_MKNODAT */
 #if !HAVE_READPASSPHRASE
