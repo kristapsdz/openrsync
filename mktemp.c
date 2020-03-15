aa031771a1a12ea69c35f856ca6a238f6a963dcb
kristapsdz <kristaps@bsd.lv>
2020-03-15 15:03:44 +0000

    Rename sun -> un (SunOS compatibility).

--- mktemp.c
+++ mktemp.c
@@ -89,7 +89,7 @@ mktemp_internalat(int pfd, char *path, int slen, enum tmpmode mode,
 	const char	 tempchars[] = TEMPCHARS;
 	unsigned int	 tries;
 	struct stat	 sb;
-	struct sockaddr_un sun;
+	struct sockaddr_un un;
 	size_t		 len;
 	int		 fd, saved_errno;
 
@@ -176,23 +176,23 @@ mktemp_internalat(int pfd, char *path, int slen, enum tmpmode mode,
 				return(-1);
 			break;
 		case MKTEMP_SOCK:
-			memset(&sun, 0, sizeof(sun));
-			sun.sun_family = AF_UNIX;
-			if ((len = strlcpy(sun.sun_path, link,
-			    sizeof(sun.sun_path))) >= sizeof(sun.sun_path)) {
+			memset(&un, 0, sizeof(un));
+			un.sun_family = AF_UNIX;
+			if ((len = strlcpy(un.sun_path, link,
+			    sizeof(un.sun_path))) >= sizeof(un.sun_path)) {
 				errno = EINVAL;
 				return(-1);
 			}
-			if (sun.sun_path[len] != '/') {
-				if (strlcat(sun.sun_path, "/",
-				    sizeof(sun.sun_path)) >=
-				    sizeof(sun.sun_path)) {
+			if (un.sun_path[len] != '/') {
+				if (strlcat(un.sun_path, "/",
+				    sizeof(un.sun_path)) >=
+				    sizeof(un.sun_path)) {
 					errno = EINVAL;
 					return(-1);
 				}
 			}
-			if (strlcat(sun.sun_path, path, sizeof(sun.sun_path)) >=
-			    sizeof(sun.sun_path)) {
+			if (strlcat(un.sun_path, path, sizeof(un.sun_path)) >=
+			    sizeof(un.sun_path)) {
 				errno = EINVAL;
 				return(-1);
 			}
@@ -210,7 +210,7 @@ mktemp_internalat(int pfd, char *path, int slen, enum tmpmode mode,
 			    FD_CLOEXEC) == -1)
 				return -1;
 #endif
-			if (bind(fd, (struct sockaddr *)&sun, sizeof(sun)) ==
+			if (bind(fd, (struct sockaddr *)&un, sizeof(un)) ==
 			    0) {
 				close(fd);
 				return(0);
